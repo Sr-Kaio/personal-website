74a310fd815678746cae083c3d453e0ff3746084
alt-spec <github.composer106@passinbox.com>
2025-02-18 23:57:28 -0300

    / page almost done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
+
 	let { children } = $props();
 </script>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_outward" />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,89 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Github, Youtube, Twitter } from 'lucide-svelte';
+</script>
+
+<style>
+    .button-text {
+        text-shadow: 4px 4px 0px var(--color-primary);
+        transition-duration: 200ms;
+    }
+    .button-icon {
+        transition-duration: 200ms;
+        color: var(--color-primary);
+    }
+    .button:hover .button-icon {
+        color: var(--color-text);
+    }
+    .button:hover .button-text {
+        text-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+        color: var(--color-background);
+    }
+</style>
+
+<main class="w-screen h-screen flex justify-center items-center flex-col p-6">
+    <div class="w-screen max-w-6xl mb-12">
+        <div class="border-b-2 border-text/15 bg-black/0 cursor-pointer w-full flex md:justify-between justify-center items-center hover:border-text/0 p-10 hover:bg-primary/70 duration-200 button">
+            <p class="font-['Inter'] font-black italic md:text-8xl text-6xl button-text">
+                /ART
+            </p>
+            <p class="text-text/50 w-64 md:block hidden">
+                Section that is focused to my artistic side
+            </p>
+            <span class="md:block hidden">
+                <span class="material-symbols-outlined button-icon ">
+                    arrow_outward
+                </span>
+            </span>
+        </div>
+        <div class="border-b-2 border-text/15 bg-black/0 cursor-pointer w-full flex md:justify-between justify-center items-center hover:border-text/0 p-10 hover:bg-primary/70 duration-200 button">
+            <p class="font-['Inter'] font-black italic md:text-8xl text-6xl button-text">
+                /DEV
+            </p>
+            <p class="text-text/50 w-64 md:block hidden">
+                Section that is focused to my developer side
+            </p>
+            <span class="md:block hidden">
+                <span class="material-symbols-outlined button-icon ">
+                    arrow_outward
+                </span>
+            </span>
+        </div>
+        <div class="border-b-2 border-text/15 bg-black/0 cursor-pointer w-full flex md:justify-between justify-center items-center hover:border-text/0 p-10 hover:bg-primary/70 duration-200 button">
+            <p class="font-['Inter'] font-black italic md:text-8xl text-6xl button-text">
+                /EDIT
+            </p>
+            <p class="text-text/50 w-64 md:block hidden">
+                Section that is focused to my video editor / motion graphics side
+            </p>
+            <span class="md:block hidden">
+                <span class="material-symbols-outlined button-icon ">
+                    arrow_outward
+                </span>
+            </span>
+        </div>
+        <div class="border-b-2 border-text/15 bg-black/0 cursor-pointer w-full flex md:justify-between justify-center items-center hover:border-text/0 p-10 hover:bg-primary/70 duration-200 button">
+            <p class="font-['Inter'] font-black italic md:text-8xl text-6xl button-text">
+                /GAME
+            </p>
+            <p class="text-text/50 w-64 md:block hidden">
+                List of my gawmes :3
+            </p>
+            <span class="md:block hidden">
+                <span class="material-symbols-outlined button-icon">
+                    arrow_outward
+                </span>
+            </span>
+        </div>
+    </div>
+    <div class="flex gap-3.5">
+        <a href="https://github.com/Sr-Kaio" class="cursor-pointer">
+            <Github class="text-text/50 hover:text-primary duration-200 size-8"/>
+        </a>
+        <a href="https://www.youtube.com/@sr_kaio" class="cursor-pointer">
+            <Youtube class="text-text/50 hover:text-primary duration-200 size-8"/>
+        </a>
+        <a href="https://x.com/SrKaio05497576" class="cursor-pointer">
+            <Twitter class="text-text/50 hover:text-primary duration-200 size-8"/>
+        </a>
+    </div>
+</main>
